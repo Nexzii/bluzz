ed2efb3e1d261bc157d5f31b9a6d10cfddc83cc5
Nexzii <maxence69430@hotmail.fr>
2025-01-02 20:36:47 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,11 @@
+const config = {
+  appId: 'com.bluzz.app',
+  appName: 'bluzz',
+  webDir: 'www',
+  server: {
+      url: 'https://maxencestievenard.ovh', // Lien vers votre site OVH
+      cleartext: false // HTTPS doit être activé
+  }
+};
+
+export default config;
